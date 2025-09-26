b77b79d652eba822027a91273232d659e24df398
gururaj1512 <gururajgurram1512@gmail.com>
2025-09-26 11:47:21 +0530

    2

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: '/api/v1',
+  baseURL: 'https://cureconnect-0oy1.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
